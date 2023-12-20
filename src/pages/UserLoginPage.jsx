29be75211f7a25b35b9cdead46fdc1125cf9e671
chandan <chandank1848@gmail.com>
2023-12-20 22:38:58 +0530

    changes in styles

--- src/pages/UserLoginPage.jsx
+++ src/pages/UserLoginPage.jsx
@@ -144,7 +144,23 @@ const UserLoginPage = () => {
                     </form>
                 </div>
 
-               
+                {/* Overlay for switching between sign-up and login */}
+                <div className="overlay-container">
+                    <div className="overlay">
+                        {/* Left overlay for login */}
+                        <div className="overlay-panel overlay-left">
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us, please login with your personal info</p>
+                            <button className="ghost" id="signIn" onClick={handleSignInClick}>Login</button>
+                        </div>
+                        {/* Right overlay for sign-up */}
+                        <div className="overlay-panel overlay-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details and start the journey with us</p>
+                            <button className="ghost" id="signUp" onClick={handleSignUpClick}>Sign Up</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
